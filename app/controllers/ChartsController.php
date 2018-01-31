91f5ef9c4c0b66444317a39098516802476cfe66
sucre.xu <sucre.xu@outlook.com>
2018-01-31 15:52:06 +0800

    ADD total called#

--- app/controllers/ChartsController.php
+++ app/controllers/ChartsController.php
@@ -15,7 +15,7 @@ class ChartsController extends ControllerBase {
 		 * $result = $connection->query ( $sql );
 		 * $statics = $result->fetchAll ( $sql );
 		 * echo JSON_encode ( $statics );
-		 * 
+		 *
 		 * but we use this, it's more simple and readable:)
 		 */
 		$callback = Callback::find ( [ 
@@ -25,7 +25,16 @@ class ChartsController extends ControllerBase {
 		] );
 		echo JSON_encode ( $callback );
 		
-		//disable the html code, so only get the JSON string above:)
+		// disable the html code, so only get the JSON string above:)
+		$this->view->disable ();
+	}
+	public function totalcalledAction() {
+		$callback = Callback::find ( [ 
+				"columns" => "distinct(qc_name) as qc,count(is_connected) as count",
+				"conditions" => "is_connected !='' and is_connected !='24小时跟踪回拨' and qc_name not in('fcqc qc','已删除','sucre xu','scarlett deng')",
+				"group" => "qc_name" 
+		] );
+		echo JSON_encode ( $callback );
 		$this->view->disable ();
 	}
 }
