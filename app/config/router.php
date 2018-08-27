df93c23a2f8fbec5edf22595d2bc691b2ce94411
sucre.xu <sucre.xu@outlook.com>
2018-08-27 16:52:35 +0800

    routes vrdScores vrdScoring

--- app/config/router.php
+++ app/config/router.php
@@ -1,7 +1,16 @@
 <?php
+/*
+ * Define custom routes. File gets included in the router service definition.
+ */
 
 $router = $di->getRouter();
 
-// Define your routes here
-
+$router->add('/vrdScoring',[
+		'controller'=>'journals',
+		'action'=>'search'
+]);
+$router->add('/vrdScores',[
+	'controller'=>'journals',
+		'action'=>'vrdScores'
+]);
 $router->handle();
